e26bde0f30877e2e5d5a3a67515ea738dfff21f6
qiwi <qiwi@qiwi.qiwi>
2025-11-20 17:21:01 +0300

    fixed malloc issue

--- levkin.dima/P4/main.cpp
+++ levkin.dima/P4/main.cpp
@@ -4,59 +4,62 @@
 
 namespace levkin {
 
-char* extend(char* old_buffer, size_t old_size, size_t new_size) {
-    char* new_buffer = (char*)malloc(new_size);
-    if (!new_buffer) return nullptr;
+char *extend(char *old_buffer, size_t old_size, size_t new_size) {
+  char *new_buffer = reinterpret_cast<char*>(malloc(new_size));
+  if (!new_buffer)
+    return nullptr;
 
-    for (size_t i = 0; i < old_size; i++) {
-        new_buffer[i] = old_buffer[i];
-    }
-    for (size_t i = old_size; i < new_size; i++) {
-        new_buffer[i] = ' ';
-    }
+  for (size_t i = 0; i < old_size; i++) {
+    new_buffer[i] = old_buffer[i];
+  }
+  for (size_t i = old_size; i < new_size; i++) {
+    new_buffer[i] = ' ';
+  }
 
-    free(old_buffer);
-    return new_buffer;
+  free(old_buffer);
+  return new_buffer;
 }
 
-char* getLine(std::istream& in, size_t& size) {
-    bool is_skip_ws = in.flags() & std::ios_base::skipws;
-    if (is_skip_ws) {
-        in >> std::noskipws;
-    }
+char *getLine(std::istream &in, size_t &size) {
+  bool is_skip_ws = in.flags() & std::ios_base::skipws;
+  if (is_skip_ws) {
+    in >> std::noskipws;
+  }
 
-    size = 5;
-    char* buffer = (char*)malloc(size);
-    if (!buffer) return nullptr;
+  size = 5;
+  char *buffer = (char *)malloc(size);
+  if (!buffer)
+    return nullptr;
 
-    size_t len = 0;
+  size_t len = 0;
 
-    char c;
-    while (in >> c) {
-        if (len == size) {
-            buffer = extend(buffer, size, size + 5);
-            if (!buffer) return nullptr;
-            size += 5;
-        }
-        buffer[len++] = c;
-    }
-
+  char c;
+  while (in >> c) {
     if (len == size) {
-        buffer = extend(buffer, size, size + 1);
-        if (!buffer) return nullptr;
-        size += 1;
+      buffer = extend(buffer, size, size + 5);
+      if (!buffer)
+        return nullptr;
+      size += 5;
     }
+    buffer[len++] = c;
+  }
 
-    buffer[len] = '\0';
+  if (len == size) {
+    buffer = extend(buffer, size, size + 1);
+    if (!buffer)
+      return nullptr;
+    size += 1;
+  }
 
-    if (is_skip_ws)
-        in >> std::skipws;
+  buffer[len] = '\0';
 
-    return buffer;
+  if (is_skip_ws)
+    in >> std::skipws;
+
+  return buffer;
 }
 
 } // namespace levkin
-
 
 int main() {
   size_t s = 0;
